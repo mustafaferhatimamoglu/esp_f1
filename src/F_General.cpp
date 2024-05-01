7aab85ccbfecabbbe540596789d8919994a5dffe
mfi-c <mustafaferhatimamoglu@gmail.com>
2024-05-01 13:34:59 +0300

    get mac

--- src/F_General.cpp
+++ src/F_General.cpp
@@ -1,35 +1,41 @@
 
 #include <Arduino.h>
-//#include "F_General.h"
+// #include "F_General.h"
 
 void Setup_Serial()
 {
-  int counter_ss = 0;
-  Serial.begin(115200);
-  while (!Serial)
-  {
-    delay(1000);
-    counter_ss++;
-  }
-  Serial.printf("[%d] Debug_SS!\n", counter_ss);
+    int counter_ss = 0;
+    Serial.begin(115200);
+    while (!Serial)
+    {
+        delay(1000);
+        counter_ss++;
+    }
+    Serial.printf("[%d] Debug_SS!\n", counter_ss);
 }
 
 int timeTick = 0;
-int get_TimeTick(){
+int get_TimeTick()
+{
     return timeTick;
 }
-void TimeTicker(void *pvParameters){
-    timeTick++;
-    delay(1000);
+void TimeTicker(void *pvParameters)
+{
+    while (1)
+    {
+        timeTick++;
+        delay(1000);
+    }
 }
-void Setup_TimeTicker(){
+void Setup_TimeTicker()
+{
     xTaskCreatePinnedToCore(
-      TimeTicker,   // Function to implement the task
-      "TimeTicker", // Name of the task
-      10000,         // Stack size in bytes
-      NULL,         // Task input parameter
-      10,           // Priority of the task
-      NULL,         // Task handle.
-      1             // Core where the task should run
-  );
+        TimeTicker,   // Function to implement the task
+        "TimeTicker", // Name of the task
+        10000,        // Stack size in bytes
+        NULL,         // Task input parameter
+        10,           // Priority of the task
+        NULL,         // Task handle.
+        0             // Core where the task should run
+    );
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,20 @@
 #include <Arduino.h>
+//A8:42:E3:56:E0:08   
 
 #include "F_General.h"
+
+#include "WiFi.h"
 
 void setup()
 {
   Setup_Serial();
   Setup_TimeTicker();
+
+    WiFi.mode(WIFI_MODE_STA);
+  Serial.println(WiFi.macAddress());
 }
 void loop()
 {
-  //Serial.println(get_TimeTick());
+  Serial.println(get_TimeTick());
   delay(3000);
 }
